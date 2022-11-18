51a9f2d21ea8960d9f9fc25b96cb53b8083e2350
Alumnos <Alumnos@LABC2PC8>
2022-11-18 11:52:05 -0600

    Agregar archivos de proyecto.

---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana_16_2.Models
+{
+    public class Movie
+    {
+      
+    }
+}
+
